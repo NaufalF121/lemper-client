ed3b9b83ff9afffa9c1e6cdf7003957e4e996223
NaufalF121 <blueshoko@gmail.com>
2024-12-01 14:22:57 +0700

    🐞 fix(tailwind configuration): fix missing default colors

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,16 +2,18 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    colors: {
-      'main': {
-        'primary':'#FDFFE2',
-        'secondary': '#83B4FF',
-        'tertiary': '#5A72A0',
-        'fourtiary': '#1A2130'
-      },
-      'negative': '#EF4C53'
+    extend: {
+      colors: {
+        'colors': {
+          'primary':'#FDFFE2',
+          'secondary': '#83B4FF',
+          'tertiary': '#5A72A0',
+          'fourtiary': '#1A2130'
+        },
+        'negative': '#EF4C53'
+      }
     },
-    extend: {},
+    
   },
   plugins: [],
 }
